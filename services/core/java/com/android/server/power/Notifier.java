f554dc8338a6648e1d0dd475b528c2b142b0bdcb
Anushek Prasal <anushekprasal@gmail.com>
2019-05-29 01:26:35 +0530

    base: Use wireless charging animation for wired charging too

--- services/core/java/com/android/server/power/Notifier.java
+++ services/core/java/com/android/server/power/Notifier.java
@@ -600,7 +600,7 @@ final class Notifier {
     /**
      * Called when wired charging has started so as to provide user feedback
      */
-    public void onWiredChargingStarted() {
+    public void onWiredChargingStarted(int batteryLevel) {
         if (DEBUG) {
             Slog.d(TAG, "onWiredChargingStarted");
         }
@@ -608,6 +608,7 @@ final class Notifier {
         mSuspendBlocker.acquire();
         Message msg = mHandler.obtainMessage(MSG_WIRED_CHARGING_STARTED);
         msg.setAsynchronous(true);
+        msg.arg1 = batteryLevel;
         mHandler.sendMessage(msg);
     }
 
@@ -771,9 +772,12 @@ final class Notifier {
         mSuspendBlocker.release();
     }
 
-    private void showWiredChargingStarted() {
+    private void showWiredChargingStarted(int batteryLevel) {
         playWirelessChargingVibration();
         playChargingStartedSound();
+        if (mStatusBarManagerInternal != null) {
+            mStatusBarManagerInternal.showChargingAnimation(batteryLevel);
+        }
         mSuspendBlocker.release();
     }
 
@@ -824,7 +828,7 @@ final class Notifier {
                     lockProfile(msg.arg1);
                     break;
                 case MSG_WIRED_CHARGING_STARTED:
-                    showWiredChargingStarted();
+                    showWiredChargingStarted(msg.arg1);
                     break;
             }
         }
